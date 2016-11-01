5df4753be39be3f539a9b1c5279792a041121f84
Nate Jacoby <natejacobymusic@gmail.com>
2016-11-01 19:14:45 +1000

    Only Selection remains visible in DOM

--- app.js
+++ app.js
@@ -16,24 +16,25 @@ function getChoices() {
 }
 
 function compare(choice1, choice2) {
-   result = " ";
-    
+    result = " ";
+    hideElements();
     if (choice1 === choice2) {
         result = "The result is a tie!";
         getChoices();
-    } else if (choice1 === "rock") {
+    } else if (choice1 === "rock") { // User selects Rock
+
         if (choice2 === "scissors") {
             result = "rock wins!";
         } else {
             result = "paper wins!"
         }
-    } else if (choice1 === "scissors") {
+    } else if (choice1 === "scissors") {    // User selects Scissors
         if (choice2 === "rock") {
             result = "rock wins!";
         } else {
             result = "scissors wins!"
         }
-    } else if (choice1 === "paper") {
+    } else if (choice1 === "paper") {       // User selects Paper
         if (choice2 === "rock") {
             result = "paper wins!";
         } else {
@@ -44,11 +45,27 @@ function compare(choice1, choice2) {
     }
 }
 
-/*   1. Select Choice
-     2. Remove other choices from view
-     3. Countdown from 3 to 1
-     4. Display Computer's choice
-     5. Display Win/Lose screen
-     6. Add to Score if won | Reset score to zero if lose
-    */
+/*           |1. Select Choice
+ 2. Remove other choices from view
+ 3. Countdown from 3 to 1
+ 4. Display Computer's choice
+ 5. Display Win/Lose screen
+ 6. Add to Score if won | Reset score to zero if lose
+ */
 
+function hideElements() {
+    var scissors = document.getElementById('scissors');
+    var paper = document.getElementById('paper');
+    var rock = document.getElementById('rock');
+
+    if (userChoice === "scissors"){
+        rock.className += ' hide';
+        paper.className += ' hide';
+    } else if (userChoice === "paper"){
+        rock.className += ' hide';
+        scissors.className += ' hide';
+    } else {
+        paper.className += ' hide';
+        scissors.className += ' hide';
+    }
+}
